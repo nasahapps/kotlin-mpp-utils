12bc1b9726f3c10d1bc399689324b5d3717125eb
Hakeem Hasan <nasahapps@gmail.com>
2019-01-02 14:23:27 -0800

    Add iOS/Mac support

---
+++ src/nativeMain/kotlin/utils/Utils.kt
@@ -0,0 +1,18 @@
+package utils
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Runnable
+import platform.darwin.dispatch_async
+import platform.darwin.dispatch_get_main_queue
+import kotlin.coroutines.CoroutineContext
+
+actual val UI: CoroutineDispatcher = object : CoroutineDispatcher() {
+    override fun dispatch(context: CoroutineContext, block: Runnable) {
+        dispatch_async(dispatch_get_main_queue()) {
+            block.run()
+        }
+    }
+}
+
+actual val IO: CoroutineDispatcher = Dispatchers.Unconfined
